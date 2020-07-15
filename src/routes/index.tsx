44529232f01f71ff853b933bbb5002b9783320db
Luan <luanfrancavieira@gmail.com>
2020-07-14 21:48:00 -0300

    style global

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,6 +9,6 @@ const Routes: React.FC = () => (
         <Route path="/" exact component={Dashboard} />
         <Route path="/repository" component={Repository} />
     </Switch>
-)
+);
 
 export default Routes;
