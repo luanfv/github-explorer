d16fae290ec90146c6a90571943b4e05d113b29c
Luan <luanfrancavieira@gmail.com>
2020-07-16 19:09:33 -0300

    styled dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,47 @@
 import React from 'react';
-import { Title } from './styles';
+import { Title, Form, Repositories } from './styles';
+import logoImg from './../../assets/logo.svg';
+import { FiChevronRight } from 'react-icons/fi';
 
 const Dashboard: React.FC = () => {
-    return <Title>Explore repositórios no Github</Title>;
+    return (
+        <>
+            <img src={logoImg} height="25px" alt="Github Explore" />
+            <Title>Explore repositórios no Github</Title>
+
+            <Form>
+                <input placeholder="Digite o nome do repositorio" />
+                <button>Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="#">
+                    <img 
+                        src="https://avatars0.githubusercontent.com/u/42809136?s=460&u=e9aaee21aa9fcba3c5ded6d94fbcde1a9846f202&v=4" 
+                        alt="Luan França Vieira"
+                    />
+                    <div>
+                        <strong>luanfv/app-rpgzando</strong>
+                        <p>App gerador de ficha de rpg D&D5e, feito com react-native.</p>
+                    </div>
+                
+                    <FiChevronRight size={20} />
+                </a>
+                <a href="#">
+                    <img 
+                        src="https://avatars0.githubusercontent.com/u/42809136?s=460&u=e9aaee21aa9fcba3c5ded6d94fbcde1a9846f202&v=4" 
+                        alt="Luan França Vieira"
+                    />
+                    <div>
+                        <strong>luanfv/cep-consult</strong>
+                        <p></p>
+                    </div>
+                
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
+        </>
+    );
 }
 
 export default Dashboard;
